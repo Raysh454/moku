40d9c680b41dc8e76fbe775a6f5d1666c028cfb4
Shaheer Ahmed <ahmedshaheer256@gmail.com>
2026-04-02 14:45:08 +0500

    Add MokuAnalyzerAdapter for moku-analyzer service integration

--- internal/app/config.go
+++ internal/app/config.go
@@ -20,6 +20,9 @@ type Config struct {
 	// A job will be deleted during cleanup if it exceeds JobRetentionTime.
 	JobRetentionTime time.Duration
 
+	// AnalyzerURL is the base URL of the moku-analyzer service
+	AnalyzerURL string
+
 	// Tracker Configuration
 	trackerCfg tracker.Config
 
@@ -41,6 +44,7 @@ func DefaultConfig() *Config {
 	return &Config{
 		StorageRoot:      "~/.config/moku",
 		JobRetentionTime: 60 * time.Minute,
+		AnalyzerURL:      "http://localhost:8080", // Default moku-analyzer service URL
 		trackerCfg: tracker.Config{
 			RedactSensitiveHeaders:  false,
 			StoragePath:             "",    // Needs to be set! (Website Directory)
